faaedeedb159e14286ed1d057ed6ae890d984d01
Joseph Lionarons <lionsamurai@gmail.com>
2019-02-24 13:19:07 -0500

    created handleSubmit and handleChange methods in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,55 @@ class Header extends Component {
     super(props);
     this.state = {
       artist: '',
-      startDate: '',
-      endDate: '',
+      startYear: '',
+      endYear: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    console.log('submit');
+    this.setState({
+      artist: '',
+      startYear: '',
+      endYear: '',
+    });
   }
 
   render() {
     return (
       <div id="header">
-        <h1>Header</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="artist">Artist:</label>
+          <input
+            type="text"
+            name="artist"
+            value={this.state.artist}
+            onChange={this.handleChange}
+            required
+          />
+          <label htmlFor="startYear">Start Year:</label>
+          <input
+            type="text"
+            name="startYear"
+            value={this.state.startYear}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="endYear">End Year:</label>
+          <input
+            type="text"
+            name="endYear"
+            value={this.state.endYear}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
