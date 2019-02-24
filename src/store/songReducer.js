0b50e8b9dcc057f3e915966c69496defa050ff68
Joseph Lionarons <lionsamurai@gmail.com>
2019-02-24 18:26:10 -0500

    added stopLoadingOnError in reducer

--- src/store/songReducer.js
+++ src/store/songReducer.js
@@ -9,6 +9,7 @@ const initialState = {
 
 const GET_SONGS_FROM_API = 'GET_SONGS_FROM_API';
 const START_LOADING = 'START_LOADING';
+const STOP_LOADING = 'STOP_LOADING';
 
 const getSongs = songs => {
   return {
@@ -23,40 +24,52 @@ export const startLoading = () => {
   };
 };
 
+const stopLoadingOnError = () => {
+  return {
+    type: STOP_LOADING,
+  };
+};
+
 export const getSongsFromAPI = (
   artistSlug,
   startYear,
   endYear
 ) => async dispatch => {
-  const { data } = await axios.get(
-    `https://itunes.apple.com/search?term=${artistSlug}&media=music&entity=song&attribute=artistTerm&limit=200`,
-    { headers: { 'Access-Control-Allow-Origin': '*' } }
-  );
-
-  let songArray = data.results;
-
-  if (startYear) {
-    songArray = songArray.filter(
-      song => song.releaseDate.slice(0, 4) >= startYear
+  try {
+    const { data } = await axios.get(
+      `https://itunes.apple.com/search?term=${artistSlug}&media=music&entity=song&attribute=artistTerm&limit=200`,
+      { headers: { 'Access-Control-Allow-Origin': '*' } }
     );
+
+    let songArray = data.results;
+
+    if (startYear) {
+      songArray = songArray.filter(
+        song => song.releaseDate.slice(0, 4) >= startYear
+      );
+    }
+
+    if (endYear) {
+      songArray = songArray.filter(
+        song => song.releaseDate.slice(0, 4) <= endYear
+      );
+    }
+
+    function compare(a, b) {
+      if (a.releaseDate < b.releaseDate) return -1;
+      if (a.releaseDate > b.releaseDate) return 1;
+      return 0;
+    }
+
+    // sorts songs in chronological order
+    songArray.sort(compare);
+
+    dispatch(getSongs(songArray));
+  } catch (error) {
+    console.error(error);
+    dispatch(stopLoadingOnError());
+    alert('Something went wrong internally. Please try again.');
   }
-
-  if (endYear) {
-    songArray = songArray.filter(
-      song => song.releaseDate.slice(0, 4) <= endYear
-    );
-  }
-
-  function compare(a, b) {
-    if (a.releaseDate < b.releaseDate) return -1;
-    if (a.releaseDate > b.releaseDate) return 1;
-    return 0;
-  }
-
-  // sorts songs in chronological order
-  songArray.sort(compare);
-
-  dispatch(getSongs(songArray));
 };
 
 const songReducer = (state = initialState, action) => {
@@ -70,6 +83,8 @@ const songReducer = (state = initialState, action) => {
       };
     case START_LOADING:
       return { ...state, loading: true };
+    case STOP_LOADING:
+      return { ...state, loading: false };
     default:
       return state;
   }
