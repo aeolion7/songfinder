bbc58ee525da46f88cdf824e450e408b22558cea
Joseph Lionarons <lionsamurai@gmail.com>
2019-02-24 15:06:58 -0500

    rendering track names from search in SongList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class SongList extends Component {
+export class SongList extends Component {
   render() {
-    return <h1>SongList</h1>;
+    return (
+      <div id="song-list">
+        {this.props.songs &&
+          this.props.songs.map(song => <p>{song.trackName}</p>)}
+      </div>
+    );
   }
 }
 
-export default SongList;
+const mapStateToProps = state => {
+  return {
+    songs: state.songReducer.songs.results,
+    songCount: state.songReducer.songs.resultCount,
+  };
+};
+
+export default connect(mapStateToProps)(SongList);
--- src/components/SongList.test.js
+++ src/components/SongList.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SongList from './SongList';
+import { SongList } from './SongList';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
