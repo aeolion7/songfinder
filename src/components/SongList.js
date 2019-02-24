0bc84655399ff4d8de49e01dc8a70e830edd3bed
Joseph Lionarons <lionsamurai@gmail.com>
2019-02-24 15:22:01 -0500

    feature: searching for artist name returns song titles, 100px img, and formatted release date

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class SongList extends Component {
+  constructor(props) {
+    super(props);
+    this.formatReleaseDate = this.formatReleaseDate.bind(this);
+  }
+
+  // formatReleaseDate takes a string in the format "1981-02-12T08:00:00Z"
+  // and returns "02-12-1981"
+  formatReleaseDate(dateStr) {
+    const year = dateStr.slice(0, 4);
+    const month = dateStr.slice(5, 7);
+    const date = dateStr.slice(8, 10);
+
+    return month + '-' + date + '-' + year;
+  }
+
   render() {
     return (
       <div id="song-list">
         {this.props.songs &&
-          this.props.songs.map(song => <p>{song.trackName}</p>)}
+          this.props.songs.map(song => (
+            <div key={song.trackId}>
+              <p>{song.trackName}</p>
+              <img src={song.artworkUrl100} alt="Track art" />
+              <p>{this.formatReleaseDate(song.releaseDate)}</p>
+            </div>
+          ))}
       </div>
     );
   }
